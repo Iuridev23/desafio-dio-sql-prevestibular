ece61275fca4044c587a6a6115e70e655d13aee4
Iuridev <iurigames23@gmail.com>
2023-08-14 21:45:21 -0300

    Criação das tabelas aluno e professor

--- desafio-prevestibular.sql
+++ desafio-prevestibular.sql
@@ -2,3 +2,23 @@
 create database prevestibular;
 use prevestibular;
 
+create table alunos(
+	matAluno int auto_increment primary key,
+	nome varchar(10),
+	sobrenome varchar(20),
+	CPF char(11) not null,
+    dataNasc date,
+	endereco varchar(255),
+	constraint unique_cpf_client unique (CPF)
+);
+
+create table professores(
+	idProf int auto_increment primary key,
+	nome varchar(10),
+	sobrenome varchar(20),
+	CPF char(11) not null,
+    dataNasc date,
+	endereco varchar(255),
+    materia varchar(15),
+	constraint unique_cpf_client unique (CPF)
+);
